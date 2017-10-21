2e482d785b1f50851574571823b03d8cb291d895
Ye Wang <whyzcandy@gmail.com>
2017-10-21 13:56:54 -0700

    add room builder

--- Shuffle/Room.cs
+++ Shuffle/Room.cs
@@ -1,4 +1,5 @@
 ﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
@@ -7,25 +8,19 @@ using System.Text;
 
 namespace Shuffle
 {
-    public class Room : GH_Component
+    public class Room
     {
-        public Room() : base("ShuttleRoom", "Room", "When connecting to the solver, updates the cells", "Shuttle", "Room")
-        {
-        }
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("19EFD237-E973-4BFD-96E1-8687CE2ED3F8"); }
-        }
+        public List<Point3d> points { get; }
+        public double size { get; }
+        public Boolean IsValid { get
+            {
+                return size > 0;
+            } }
 
-        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        public Room(List<Point3d> points, double size)
         {
-            pManager.AddPointParameter("cells", "cells", "Cells that belong to the room", GH_ParamAccess.list);
-            pManager.AddNumberParameter("minSize", "minSize", "Minimum size of the room", GH_ParamAccess.item);
-        }
-
-        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-        {
-            pManager.AddVectorParameter("result cells", "cells", "Cells that the room wants to grow", GH_ParamAccess.list);
+            this.points = points;
+            this.size = size;
         }
 
         private int encode(Point3d point, int maxX)
@@ -34,27 +29,21 @@ namespace Shuffle
             var Y = (int)Math.Round(point.Y) + 10;
             return Y * maxX + X;
         }
-        protected override void SolveInstance(IGH_DataAccess DA)
+
+        public List<Point3d> step()
         {
-            List<Point3d> points = new List<Point3d>();
-            double size = 0.0;
-
-            if (!DA.GetDataList(0, points)) return;
-            if (!DA.GetData(1, ref size)) return;
-
             List<Point3d> result = new List<Point3d>();
             if (points.Count > size)
             {
-                DA.SetDataList(0, result);
-                return;
+                return result;
             }
             int maxX = 0;
             foreach (Point3d point in points)
             {
                 if (point.X > maxX)
                 {
-                    maxX = (int) Math.Round(point.X);
-                }  
+                    maxX = (int)Math.Round(point.X);
+                }
             }
             maxX += 10;
             HashSet<int> grids = new HashSet<int>();
@@ -64,7 +53,7 @@ namespace Shuffle
             }
             foreach (Point3d point in points)
             {
-                foreach (int stepX in new int[3]{-1, 0, 1}) {
+                foreach (int stepX in new int[3] { -1, 0, 1 }) {
                     foreach (int stepY in new int[3] { -1, 0, 1 })
                     {
                         Point3d newPoint = point + new Point3d(stepX, stepY, 0);
@@ -81,7 +70,69 @@ namespace Shuffle
                     }
                 }
             }
-            DA.SetDataList(0, result);
+            return result;
+        }
+    }
+
+    public class RoomGoo : GH_Goo<Room>
+    {
+        public override bool IsValid
+        {
+            get
+            {
+                return Value != null && Value.IsValid;
+            }
+        }
+        
+        public RoomGoo()
+        {
+            this.Value = null;
+        }
+
+        public RoomGoo(Room room)
+        {
+            Value = room;
+        }
+
+        public override string TypeDescription
+        {
+            get
+            {
+                return "This is a steppable object";
+            }
+        }
+
+        public override string TypeName
+        {
+            get
+            {
+                return "Room";
+            }
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new RoomGoo(new Room(Value.points, Value.size));
+        }
+
+        public override string ToString()
+        {
+            return "Room";
+        }
+    }
+
+    public class RoomParameter : GH_Param<RoomGoo>
+    {
+        public RoomParameter() : base("Room", "room", "Room parameter that contains a step function", "Shuffle", "Parameters", GH_ParamAccess.item)
+        {
+        }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("3ED8C77E-3284-4FEC-B7E3-4155D52FBDCE");
+            }
         }
     }
 }
