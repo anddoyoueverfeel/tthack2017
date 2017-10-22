28defa9790c599d4168b2fe4f93c11d02f65e1ef
anddoyoueverfeel <mjb28@njit.edu>
2017-10-22 10:50:25 -0400

    block builder and block object definition

---
+++ Shuffle/NodeBlockBuilder.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Shuffle
+{
+    public class NodeBlockBuilder : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public NodeBlockBuilder()
+          : base("Block Builder", "B Build",
+              "Description",
+              "Shuffle", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            //to do: make the thing smart enough to calculate its own corner points..
+            pManager.AddCurveParameter("ObjectGeom", "OGeom", "Curves that represent the object itself", GH_ParamAccess.list);
+            pManager.AddPointParameter("ObjectCorner1", "OC1", "The bottom left corner of the object", GH_ParamAccess.item);
+            pManager.AddPointParameter("ObjectCorner2", "OC2", "The top right corner of the object", GH_ParamAccess.item);
+
+            pManager.AddCurveParameter("ClearGeom", "CGeom", "Curves that represent clearances that must be maintained around the object", GH_ParamAccess.list);
+            pManager.AddPointParameter("ClearCorner1", "CC1", "The bottom left corner of the clearance box", GH_ParamAccess.item);
+            pManager.AddPointParameter("ClearCorner2", "CC2", "The top right corner of the clearance box", GH_ParamAccess.item);
+
+            pManager.AddPointParameter("Center", "C", "Center-ish point of the object, needed for internal stuff", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Priority","P","Priority determines how powerfully this object's clearances need to be enforced", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddParameter(new ShuffleBlockParam(), "Block", "B", "A shuffle block for inserting into a shuffle room", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<Curve> objGeometry = new List<Curve>();
+            Point3d objectCorner1 = new Point3d();
+            Point3d objectCorner2 = new Point3d();
+
+            List<Curve> clearGeometry = new List<Curve>();
+            Point3d clearanceCorner1 = new Point3d();
+            Point3d clearanceCorner2 = new Point3d();
+
+            Point3d centerPoint = new Point3d();
+            int priority = -1;
+
+            if (!DA.GetDataList(0, objGeometry)) return;
+            if (!DA.GetData(1, ref objectCorner1)) return;
+            if (!DA.GetData(2, ref objectCorner2)) return;
+
+            if (!DA.GetDataList(3, objGeometry)) return;
+            if (!DA.GetData(4, ref clearanceCorner1)) return;
+            if (!DA.GetData(5, ref clearanceCorner2)) return;
+
+            if (!DA.GetData(6, ref centerPoint)) return;
+            if (!DA.GetData(7, ref priority)) return;
+
+            Dictionary<string, int> cornerOffsets = new Dictionary<string, int>();
+            cornerOffsets.Add("bottomLeftX", -2);
+            cornerOffsets.Add("bottomLeftY", -2);
+            cornerOffsets.Add("topRightX", 2);
+            cornerOffsets.Add("topRightY", 2);
+
+            ShuffleBlock myBlock = new ShuffleBlock(centerPoint, cornerOffsets, priority, objGeometry, clearGeometry);
+
+            DA.SetData(0, myBlock);
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b40f76a3-380d-4064-b3be-f39accf07b13"); }
+        }
+    }
+}
--- Shuffle/ShuffleBlock.cs
+++ Shuffle/ShuffleBlock.cs
@@ -1,22 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Rhino.Geometry;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 
 namespace Shuffle
 {
     public class ShuffleBlock
     {
+        //fields
+        private Point3d m_centerPoint;
+        private Dictionary<string, int> m_cornerOffsets;
+        private int m_rotationDegrees; //for now this is only intervals of 90
+        private int m_priority;
+        private List<Curve> m_geomOfObject;
+        private List<Curve> m_gGeomOfClearance;
+
+
+        //constructors
+        public ShuffleBlock()
+        {
+
+        }
+
+        public ShuffleBlock(Point3d centerPoint, Dictionary<string, int> cornerOffsets, int priority, List<Curve> GeomOfObject, List<Curve> GeomOfClearance)
+        {
+            this.m_centerPoint = centerPoint;
+            this.m_cornerOffsets = cornerOffsets;
+            this.m_priority = priority;
+            this.m_geomOfObject = GeomOfObject;
+            this.m_gGeomOfClearance = GeomOfClearance;
+        }
+
+        public ShuffleBlock(ShuffleBlock oldShuffleBlock)
+        {
+            //all properties get set the same as in the oldShuffleBlock
+            this.m_centerPoint = oldShuffleBlock.centerPoint;
+        }
+
+
+        public virtual ShuffleBlock Duplicate()
+        {
+            return new ShuffleBlock(this);
+        }
+
+
+        //properties
+        public Point3d centerPoint { get { return m_centerPoint; } }
+        public int priority { set; get; }
+        public virtual bool IsValid { get; }
+
+
+        //methods
         public Dictionary<string, Cell> GetBoundingBox()
         {
             Dictionary<string, Cell> temp = new Dictionary<string, Cell>();
             temp.Add("min", new Cell());
             temp.Add("max", new Cell());
             return temp;
-
-
         }
+
 
         public Dictionary<string, Cell> GetObjectBoundingBox()
         {
@@ -26,21 +69,168 @@ namespace Shuffle
             return temp;
         }
 
-
-        public Point3d centerPoint {
-            set; get;
-        }
-
-
-        
-
-
-        public int priority { set; get; }
-
+        //to do: does this actually do the moving?
         public void setDirection(int deltaX, int deltaY)
         {
 
         }
 
+
     }
+
+
+    public class ShuffleBlockGoo : GH_Goo<ShuffleBlock>
+    {
+
+        public ShuffleBlockGoo()
+        {
+            this.Value = new ShuffleBlock();
+        }
+
+        public ShuffleBlockGoo(ShuffleBlock shuffleBlock)
+        {
+            if (shuffleBlock == null)
+            {
+                this.Value = new ShuffleBlock();
+            }
+            this.Value = shuffleBlock;
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return DuplicateShuffleBlock();
+        }
+
+        public ShuffleBlockGoo DuplicateShuffleBlock()
+        {
+
+            ShuffleBlock foobar = new ShuffleBlock();
+
+            if (Value == null)
+            {
+                foobar = new ShuffleBlock();
+            }
+
+            else
+            {
+                foobar = Value.Duplicate();
+            }
+
+            return new ShuffleBlockGoo(foobar);
+
+        }
+
+        #region properties
+        public override bool IsValid
+        {
+            get
+            {
+                if (Value == null) { return false; }
+                return Value.IsValid;
+            }
+        }
+
+        public override string IsValidWhyNot
+        {
+            get
+            {
+                if (Value == null) { return "No internal Shuffle block instance"; }
+                if (Value.IsValid) { return string.Empty; }
+                return "Invalid Shuffle Block instance"; //Todo: beef this up to be more informative.
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Value == null)
+                return "A invalid, null, or empty shuffle block... what do you want to know?";
+            else
+                return Value.ToString();
+        }
+
+        public override string TypeName
+        {
+            get { return ("Shuffle Block"); }
+        }
+
+        public override string TypeDescription
+        {
+            get { return ("An instance of a shuffle block"); }
+        }
+        #endregion
+
+
+        public override bool CastTo<Q>(ref Q target)
+        {
+
+            if (typeof(Q).IsAssignableFrom(typeof(ShuffleBlock)))
+            {
+                if (Value == null)
+                    target = default(Q);
+                else
+                    target = (Q)(object)Value;
+                return true;
+            }
+            target = default(Q);
+            return false;
+        }
+
+        public override bool CastFrom(object source)
+        {
+            if (source == null) { return false; }
+
+            //Cast from BoatShell
+            if (typeof(ShuffleBlock).IsAssignableFrom(source.GetType()))
+            {
+                Value = (ShuffleBlock)source;
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+
+    public class ShuffleBlockParam : GH_Param<ShuffleBlockGoo>
+    {
+        public ShuffleBlockParam()
+          : base(new GH_InstanceDescription("Shuffle blocks!", "These things live in rooms", "Maintains a collection of shuffle blocks", "Shuffle", "Params"))
+        {
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null; //Todo, provide an icon.
+            }
+        }
+
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                // If you want to provide this parameter on the toolbars, use something other than hidden.
+                return GH_Exposure.hidden;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("588F6617-3134-4F5A-8E72-6B8D8F42E75C");
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
 }
